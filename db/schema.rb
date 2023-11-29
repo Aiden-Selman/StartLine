99f4aea806a438a8ba1602c34815bdf5451ad973
Charles Abueg <121977808+cdabueg@users.noreply.github.com>
2023-11-29 14:54:06 -0600

    Seeds can now print game details from API.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_29_153246) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_191806) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_29_153246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price"
+    t.text "description"
   end
 
   create_table "genres", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,47 @@
+require 'rest-client'
+
+NUMBER_OF_GAMES = 2
+
 AdminUser.delete_all
 Game.delete_all
 Platform.delete_all
 Genre.delete_all
 
-NUMBER_OF_GAMES = 100
+# Platform.create(platform_name: "PC")
+# Platform.create(platform_name: "Playstation")
+# Platform.create(platform_name: "Xbox")
+# Platform.create(platform_name: "Nintendo")
 
-Platform.create(platform_name: "PC")
-Platform.create(platform_name: "Playstation")
-Platform.create(platform_name: "Xbox")
-Platform.create(platform_name: "Nintendo")
+# Fetches the data from the API.
+def fetch_data
+  games_list = JSON.parse(File.read("db/steamgames.json"))
 
-NUMBER_OF_GAMES.times do |i|
-  # Step 1: Get game info from API call.
-  # Step 2: Check that games genre. If it doesn't exist in the database, add it, if it does, skip it.
-  # Step 3: Create the game with the correct data and connections to other tables (platform and genre).
-  game_info = URI.open("https://store.steampowered.com/api/appdetails?appids=#{i + 1}")
-  break
-  # puts game_id, game_info
-  # genre = Genre.create(genre_name: )
+  games_list["applist"]["apps"].last(NUMBER_OF_GAMES).each do |game|
+    appid = game["appid"].to_s
+    fetch = RestClient.get("https://store.steampowered.com/api/appdetails?appids=#{appid}")
+    data = JSON.parse(fetch)
+
+    puts "Game Name: #{data[appid]['data']['name']}"
+    puts "Steam App ID: #{data[appid]['data']['steam_appid']}"
+    puts "Required Age: #{data[appid]['data']['required_age']}"
+    puts "Short Description: #{data[appid]['data']['short_description']}"
+    puts "Price: #{data[appid]&.dig('data', 'price_overview', 'initial') || 0}"
+    puts "Platforms: #{data[appid]['data']['platforms'].map { |platform| platform[0] }.join(', ')}"
+    puts "Genres: #{data[appid]['data']['genres'].map { |genre| genre['description'] }.join(', ')}"
+    puts "Release Date: #{data[appid]['data']['release_date']['date']}"
+    puts "----------------------------------------"
+  end
 end
+fetch_data
 
+# NUMBER_OF_GAMES.times do |i|
+#   # Step 1: Get game info from API call.
+#   # Step 2: Check that games genre. If it doesn't exist in the database, add it, if it does, skip it.
+#   # Step 3: Create the game with the correct data and connections to other tables (platform and genre).
+#   game_info = URI.open("https://store.steampowered.com/api/appdetails?appids=#{i + 10}")
+#   break
+#   # puts game_id, game_info
+#   # genre = Genre.create(genre_name: )
+# end
 
-
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
