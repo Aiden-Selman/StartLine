290eba1e004ffe3ce22dc0ea1f1a9ba9c64b69c7
Ectectraa <selman.aiden@gmail.com>
2023-11-29 18:30:27 -0600

    Fixed seeds.rb
    Fixed the platform_id and genre_id fields when creating a game. Price still is always 0 however.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@ Game.delete_all
 Platform.delete_all
 Genre.delete_all
 
-# Platform.create(platform_name: "PC")
+# Platform.create(platform_name: "Windows")
+# Platform.create(platform_name: "Linux")
+# Platform.create(platform_name: "Mac")
 # Platform.create(platform_name: "Playstation")
 # Platform.create(platform_name: "Xbox")
 # Platform.create(platform_name: "Nintendo")
@@ -35,30 +37,23 @@ def fetch_data
     platform = Platform.find_or_create_by(platform_name: data[appid]["data"]["platforms"].map { |platform| platform[0] }.join(", "))
     genre = Genre.find_or_create_by(genre_name: data[appid]["data"]["genres"].map { |genre| genre["description"] }.join(", "))
 
+    puts data[appid]['data']
+    exit
+
     if platform && genre
       Game.create(
         game_name:          data[appid]["data"]["name"],
-        # stean_appid:        appid.to_i,
+        # stean_appid:        appid.to_int,
         rating:             data[appid]["data"]["required_age"],
-        description:  data[appid]["data"]["short_description"],
-        price:              data[appid]&.dig("data", "price_overview", "initial") || 0,
-        # platform:          platform,
-        # genre:             genre,
-        release_date:       data[appid]["data"]["release_date"]["date"]
+        description:        data[appid]["data"]["short_description"],
+        price:              data[appid]&.dig("data", "price_overview", "final") || 0,
+        platform_id:        platform.id,
+        genre_id:           genre.id,
+        release_date:       data[appid]["data"]["release_date"]["date"],
       )
     end
   end
 end
 fetch_data
-
-# NUMBER_OF_GAMES.times do |i|
-#   # Step 1: Get game info from API call.
-#   # Step 2: Check that games genre. If it doesn't exist in the database, add it, if it does, skip it.
-#   # Step 3: Create the game with the correct data and connections to other tables (platform and genre).
-#   game_info = URI.open("https://store.steampowered.com/api/appdetails?appids=#{i + 10}")
-#   break
-#   # puts game_id, game_info
-#   # genre = Genre.create(genre_name: )
-# end
 
 AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
