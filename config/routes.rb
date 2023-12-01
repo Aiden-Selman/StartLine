a29d6251dacd0577162f4830872e85282a4d49d4
Ectectraa <selman.aiden@gmail.com>
2023-12-01 10:19:00 -0600

    Added navigation
    Added a menu to navigate between all pages as well as the link_to helper method to link the index to the show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'platforms/index'
-  get 'platforms/show'
-  get 'genres/index'
-  get 'genres/show'
-  get 'games/index'
-  get 'games/show'
+  root "games#index"
+
+  resources :games
+  resources :genres
+  resources :platforms
+  # get 'platforms/index'
+  # get 'platforms/show'
+  # get 'genres/index'
+  # get 'genres/show'
+  # get 'games/index'
+  # get 'games/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
