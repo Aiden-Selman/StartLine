db22105a37ede2707a987121708bcc6ac73e2a10
Ectectraa <selman.aiden@gmail.com>
2023-12-01 10:05:29 -0600

    Added viewing to views
    Added the ability to see data from the database in the games, genres, and platforms views.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,11 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
   end
 
   def show
+    @game = Game.find(params[:id])
+    @genre = Genre.find(@game.genre_id)
+    @platform = Platform.find(@game.platform_id)
   end
 end
