4b2b852a520a6216290807d777f2be3b47610372
Ectectraa <selman.aiden@gmail.com>
2023-12-14 19:57:30 -0600

    Added orders to active admin and skeleton code for creating an order

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -14,6 +14,13 @@ class CheckoutController < ApplicationController
 
   def create
     flash[:notice] = params
+    # Order.create()
+    # current_user.id
+    # session[:shopping_cart]
+    # params[:quantity]
+    # params[:province]
+    # params[:total_price]
+
     redirect_to root_path
   end
 end
